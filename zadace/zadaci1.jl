3c0aa64c458b1071159a9806b0856ba9e3637a0c
Sara <sara.lazarusic@gmail.com>
2024-05-13 22:01:51 +0200

    zadaci 1 i 2

--- zadace/zadaci1.jl
+++ zadace/zadaci1.jl
@@ -1,10 +1,18 @@
-##
+## 1
 x = y = 1
 x
 y
+# odgovor: x i y iznose 1
 
+## 2
+15 = a
+# dobijemo error
 
-## funkcija
+## 3
+xy
+# xy je oznaka neke druge varijable, a x*y umnozak
+
+## 4 funkcija
 function volumen(a, objekt)
    if (objekt == "sfera")
       return (4/3)*pi*a^3
@@ -18,3 +26,39 @@ end
 volumen(2, "kocka")
 volumen(2, "sfera")
 volumen(2, "nesto")
+
+## 5
+function myAbs(x) 
+   if (x < 0) 
+      return -x
+   else 
+      return x
+   end
+end
+
+myAbs(8)
+myAbs(-8)
+
+## 6
+function dist(x1, y1, x2, y2) 
+   return sqrt((x2-x1)^2 + (y2-y1)^2)
+end
+
+dist(1, 2, -2, 6)
+
+## 7
+for i=1:30
+   if (i%3 != 0)
+      println(i)
+   end
+end
+
+## 8
+cnt = 0
+for i="Volim studirati u Zagrebu"
+   if (i == 'a' || i == 'A')
+      cnt += 1
+   end
+end
+
+println(cnt)
---
+++ zadace/zadaci2.jl
@@ -0,0 +1,34 @@
+## 1
+function transform(temp, type)
+   if (type == "Fahrenheit")
+      return temp*1.8+32
+   end
+   if (type == "Kelvin")
+      return temp+274.15
+   end
+end
+
+transform(10, "Fahrenheit")
+transform(10, "Kelvin")
+
+## 2
+# a, b = katete, c = hipotenuza
+function area(a, b, c)
+   if (c == -1) # nepoznata hipotenuza
+      return a*b*0.5
+   end
+   a = max(a, b) # nepoznata neka kateta, u a spremamo poznatu
+   return a*sqrt(c^2-a^2)*0.5
+end
+
+area(4, 6, -1)
+area(-1, 4, 5)
+area(6, -1, 10)
+
+## 3
+function area2(a, b, c)
+   s = 0.5*(a+b+c)
+   return sqrt(s*(s-a)*(s-b)*(s-c))
+end
+
+area2(5, 5, 6)
